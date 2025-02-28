20c2edf50026662617bdff5162cc66b1a1bfe308
Matheus Castro <113482506+MatheusCastroo@users.noreply.github.com>
2025-02-28 07:54:46 -0300

    Update calculo.php

--- calculadora_solar/calculo.php
+++ calculadora_solar/calculo.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($potencia) || !is_numeric($potencia) || $potencia <= 0) {
         echo "Erro: Informe uma potência válida!";
         exit;
-    }
+   }
 
     // Validação do modelo de bateria
     if ($modelo_bateria == "null" || empty($modelo_bateria)) {
@@ -30,8 +30,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //Verifica se encontrou a bateria no banco
     if ($stmt->rowCount() > 0) {
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        echo "Número do AH da bateria selecionada: " . $row['capacidade_ah'];
+        if ($modelo_bateria == $stmt) {
+            echo "Número do AH da bateria selecionada: " . $row['capacidade_ah'];
+        }
+        else {
+            echo 'teste';
+        }
+        
     } else {
         echo "Erro: Modelo de bateria não encontrado!";
     }
+    /*O código está validando a descrição da bateria do formulário com a coluna de AH, ele deve validar o descrição com a coluna de descrição e retornar o AH*/
 }
+ 
